b0e8ae5773f72a20e4feaf962a8d2d7ce1cd9b4c
Max batleforc <maxleriche.60@gmail.com>
2024-10-23 22:58:23 +0200

    fix: try to sync

--- 0.terraform/zitadel/init.tf
+++ 0.terraform/zitadel/init.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     vault = {
-      source = "hashicorp/vault"
+      source  = "hashicorp/vault"
       version = "4.4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.33.0"
     }
   }
 }
@@ -20,16 +24,39 @@ variable "masterkey" {
 }
 
 provider "vault" {
-    address = "https://localhost:8200"
+  address = "https://localhost:8200"
+}
+
+provider "kubernetes" {
+  config_path = "../../kubeconfig.yaml"
 }
 
 resource "vault_generic_secret" "zitadel_pg" {
   path = "secret/zitadel/pg"
   data_json = jsonencode(
     {
-        password_admin = var.adminpassword
-        password_user = var.userpassword
-        masterkey = var.masterkey
+      password_admin = var.adminpassword
+      password_user  = var.userpassword
+      masterkey      = var.masterkey
     }
   )
+}
+
+# Because zitadel doesn't handle everything in a way where i can use the vault provider, i have to use the kubernetes provider
+resource "kubernetes_secret" "zitadel_secret" {
+  metadata {
+    name      = "zitadel-zitadel"
+    namespace = "zitadel"
+  }
+  data = {
+    "masterkey"   = var.masterkey
+    "config-yaml" = <<EOT
+      Database:
+        Postgres:
+          User:
+            Password: "${var.userpassword}"
+          Admin:
+            Password: "${var.adminpassword}"
+    EOT
+  }
 }
